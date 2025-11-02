7edef82136f57c2246d31f8abdb860e42f06fa97
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2025-11-02 19:04:17 -0400

    chore: remove todo

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -4,24 +4,6 @@ import { createRouter } from "./router";
 
 const router = createRouter();
 
-// TODO: Add global test
-// TODO Test error boundary
-/**
- * TODO: Something
- * - [ ] Task 1
- * - [ ] Task 2
- *
- * description
- */
-
-/**
- * TODO Something else
- * - [ ] Task A
- * - [ ] Task B
- *
- * description
- */
-
 export const App = () => {
 	return (
 		<React.StrictMode>
