b7b80ba5d4b0f1e5e506aaa442fa34632519575a
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2025-11-02 19:04:23 -0400

    chore: add todo

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -4,6 +4,24 @@ import { createRouter } from "./router";
 
 const router = createRouter();
 
+// TODO: Add global test
+// TODO Test error boundary
+/**
+ * TODO: Something
+ * - [ ] Task 1
+ * - [ ] Task 2
+ *
+ * description
+ */
+
+/**
+ * TODO Something else
+ * - [ ] Task A
+ * - [ ] Task B
+ *
+ * description
+ */
+
 export const App = () => {
 	return (
 		<React.StrictMode>
